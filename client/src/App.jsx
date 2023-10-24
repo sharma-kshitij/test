61e58177c8d2193a95dd7215abe4fd015fef316b
Kshitij Sharma <sharmakshitij250@gmail.com>
2023-10-24 16:47:00 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+    // console.log(name, email, password);
+
+    const response = await fetch("http://localhost:8080/api/register", {
+      method:"POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json()
+    console.log(data)
+  };
+
+  return (
+    <>
+      <h1>Register</h1>
+      <form onSubmit={(e) => registerUser(e)}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => {
+            setname(e.target.value);
+          }}
+        />
+        <br />
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => {
+            setemail(e.target.value);
+          }}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => {
+            setpassword(e.target.value);
+          }}
+        />
+        <br />
+        <input type="submit" value={"Register"} />
+      </form>
+    </>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const User = require("./models/user.model")
+
+const port = 8080;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://sharmakshitij250:admin@cluster0.nfzuvjm.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/api/register", async (req, res) => {
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json({ status: "ok" });
+  } catch (error) {
+    res.json({ status: "error", error });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`The server is running on port ${port}`);
+});
